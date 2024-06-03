7a44ef10698c234bca4f33f3077310f82b96e7c2
JPomorski <kuba.pomor@gmail.com>
2024-06-03 16:45:40 +0200

    Added New Convert Recipe

--- challenge/pattern-18-interpreter/src/Main.java
+++ challenge/pattern-18-interpreter/src/Main.java
@@ -5,24 +5,28 @@ public class Main {
         Context itemList = new Context();
         itemList.addItem(1, "Wood");
         itemList.addItem(2, "Iron");
+        itemList.addItem(3, "Planks");
         itemList.addItem(4, "Sticks");
         itemList.addItem(5, "Pickaxe Head");
+        itemList.addItem(9, "Iron Nuggets");
         itemList.addItem(10, "Pickaxe");
 
         itemList.addItem(-1, "Invalid recipe");
 
-        Item wood1 = new Item(1);
-        Item wood2 = new Item(1);
+        Item wood = new Item(1);
+        Recipe planks1 = new Recipes.Convert(wood);
+        Recipe planks2 = new Recipes.Convert(wood);
+        System.out.println("Item crafted: " + itemList.getItem(planks1.evaluate()));
 
-        Recipes.Combine sticks = new Recipes.Combine(List.of(wood1, wood2));
+        Recipe sticks = new Recipes.Combine(List.of(planks1, planks2));
         System.out.println("Item crafted: " + itemList.getItem(sticks.evaluate()));
 
         Item iron1 = new Item(2);
         Item iron2 = new Item(2);
         Item iron3 = new Item(2);
 
-        Recipes.Combine pickaxeHead = new Recipes.Combine(List.of(iron1, iron2, iron3));
-        Recipes.Combine pickaxe = new Recipes.Combine(List.of(sticks, pickaxeHead));
+        Recipe pickaxeHead = new Recipes.Combine(List.of(iron1, iron2, iron3));
+        Recipe pickaxe = new Recipes.Combine(List.of(sticks, pickaxeHead));
 
         System.out.println("Item crafted: " + itemList.getItem(pickaxeHead.evaluate()));
         System.out.println("Item crafted: " + itemList.getItem(pickaxe.evaluate()));
--- challenge/pattern-18-interpreter/src/Recipes.java
+++ challenge/pattern-18-interpreter/src/Recipes.java
@@ -4,18 +4,35 @@ public class Recipes {
     public static class Combine implements Recipe {
         private final int id;
 
-        public Combine(List<Recipe> ingredients) {
+        public Combine(List<Recipe> items) {
             StringBuilder code = new StringBuilder();
 
-            for(Recipe ingredient : ingredients) {
-                code.append(ingredient.evaluate());
+            for(Recipe item : items) {
+                code.append(item.evaluate());
             }
 
             this.id = switch(code.toString()) {
-                case "11" -> 4;
+                case "33" -> 4;
                 case "45" -> 10;
                 case "222" -> 5;
-                case "111" -> 6;
+                case "333" -> 6;
+                default -> -1;
+            };
+        }
+
+        @Override
+        public int evaluate() {
+            return id;
+        }
+    }
+
+    public static class Convert implements Recipe {
+        private final int id;
+
+        public Convert(Recipe item) {
+            this.id = switch (item.evaluate()) {
+                case 1 -> 3;
+                case 2 -> 9;
                 default -> -1;
             };
         }
